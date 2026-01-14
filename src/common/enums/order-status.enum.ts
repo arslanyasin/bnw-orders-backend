00686e7e38445c261716695186a2c356136d0ab9
Arslan Yasin <arslanyasin1993@gmail.com>
2026-01-14 16:53:25 +0500

    order status updated

--- src/common/enums/order-status.enum.ts
+++ src/common/enums/order-status.enum.ts
@@ -2,6 +2,7 @@ export enum OrderStatus {
   PENDING = 'pending',
   CONFIRMED = 'confirmed',
   CANCELLED = 'cancelled',
+  RETURNED = 'returned',
   PROCESSING = 'processing',
   DISPATCH = 'dispatched',
   DELIVERED = 'delivered',
